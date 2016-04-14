7dd4351b9cbf1c84f432132f1cef0f62b6e196be
393_HPE580jp <393@bascule.co.j>
2016-04-14 11:24:23 +0900

    ドラえもんカラーフェード

--- js/saku3d/util/ImageFadeUtil.js
+++ js/saku3d/util/ImageFadeUtil.js
@@ -20,16 +20,17 @@ ImageFadeUtil.prototype = {
     // this.imgdata = this.ctx.getImageData(0, 0, canvas.width, canvas.height);
     // this.data = this.imgdata.data;
 
-    this.ctx.drawImage(ImageLoader.images["images/dorami.png"],0,0)
-    this.update();
     this.alpha = 1;
     this.counter = 0;
+    this.imageCounter = 0;
     this.imageArray = [];
     this.imageArray.push(ImageLoader.images["images/dora.png"])
     this.imageArray.push(ImageLoader.images["images/dorami.png"])
+    this.image = ImageLoader.images["images/dora.png"];
     setInterval((function(){
       this.counter++;
     }).bind(this),5000)
+    this.update();
   },
   fadeIn:function(){
     if(this.alpha >= 1) return;
@@ -39,7 +40,12 @@ ImageFadeUtil.prototype = {
   fadeOut:function(){
     if(this.alpha <= 0) return;
     this.alpha -= this.CLOCK.getDelta() * 0.001;
-    if(this.alpha < 0) this.alpha = 0;
+    if(this.alpha < 0){
+      this.alpha = 0;
+      this.counter++;
+      this.imageCounter++;
+      this.image = this.imageArray[this.imageCounter%2]
+    }
   },
   update: function () {
     if(this.counter%2==0){
@@ -51,18 +57,7 @@ ImageFadeUtil.prototype = {
     this.ctx.fillStyle = "rgb(255, 255, 255)";
     this.ctx.fillRect(0,0,this.imageWidth,this.imageHeight)
     this.ctx.globalAlpha = this.alpha;
-    this.ctx.drawImage(ImageLoader.images["images/dorami.png"],0,0)
-    // for (var x = 0; x < this.imageWidth; x++) {
-    //   for (var y = 0; y < this.imageHeight; y++) {
-    //     var r = this.simplex.noise3D(x / 60, y / 60, t/100) * 0.5 + 0.5;
-    //     var g = this.simplex.noise3D(x / 40, y / 140, t/100) * 0.5 + 0.5;
-    //     this.data[(x + y * 256) * 4 + 0] = r * 80;
-    //     this.data[(x + y * 256) * 4 + 1] = (g) * 200;
-    //     this.data[(x + y * 256) * 4 + 2] = (r+g) * 100;
-    //     this.data[(x + y * 256) * 4 + 3] = 255;
-    //   }
-    // }
-    // this.ctx.putImageData(this.imgdata, 0, 0);
+    this.ctx.drawImage(this.image,0,0);
     return this.ctx.canvas;
   }
 };
